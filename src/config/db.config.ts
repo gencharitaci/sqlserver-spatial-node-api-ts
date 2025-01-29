aa59dcf72183619a5725711c7f33c0dc1e8ca770
Adem Kurtipek <gencharitaci@gmail.com>
2025-01-29 10:11:05 -0500

    Updated db conf

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -7,15 +7,30 @@ declare module "fastify" {
   }
 }
 
-export default fp(async (fastify) => {
-  const connectionString = process.env.MSSQL_CONNECTION;
+const sqlConfig = {
+  user: "DB_USER",
+  password: "DB_PASSWORD",
+  database: "DB_NAME",
+  server: "DB_SERVER",
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: false,
+    trustServerCertificate: true 
+  }
+}
 
-  if (!connectionString) {
-    fastify.log.error("MSSQL_CONNECTION is not defined in the .env file");
-    throw new Error("Missing MSSQL_CONNECTION environment variable");
+export default fp(async (fastify) => {
+
+  if (!sqlConfig) {
+    fastify.log.error("Sql configuration error");
+    throw new Error("Error: Environment variables");
   }
 
-  const pool = new sql.ConnectionPool(connectionString);
+  const pool = new sql.ConnectionPool(sqlConfig);
 
   try {
     await pool.connect();
